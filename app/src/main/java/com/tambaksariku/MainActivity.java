c75663cd5f95a17fd28857fee759b4a7d6abd0c7
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2017-08-19 14:42:20 +0700

    update

--- app/src/main/java/com/tambaksariku/MainActivity.java
+++ app/src/main/java/com/tambaksariku/MainActivity.java
@@ -1,9 +1,7 @@
 package com.tambaksariku;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.v4.app.FragmentManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import static com.tambaksariku.R.layout.fragment_wisata;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -58,7 +59,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
             return true;
         }
 
@@ -71,14 +72,18 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_wisata) {
+
+            WisataFragment wisata = new WisataFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.container, wisata).commit();
+
         } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+            Toast.makeText(this, "Galeri", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_tentang) {
+            Toast.makeText(this, "Tentang", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_logout) {
+            Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- app/src/main/java/com/tambaksariku/WelcomeFragment.java
+++ app/src/main/java/com/tambaksariku/WelcomeFragment.java
@@ -1,23 +1,20 @@
 package com.tambaksariku;
 
-import android.content.Intent;
-import android.media.Image;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import static android.R.attr.id;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class WisataFragment extends Fragment {
+public class WelcomeFragment extends Fragment {
 
-    public WisataFragment() {
+
+    public WelcomeFragment() {
         // Required empty public constructor
     }
 
@@ -25,9 +22,8 @@ public class WisataFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        
-        return inflater.inflate(R.layout.fragment_wisata, container, false);
-
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false);
     }
 
 }
